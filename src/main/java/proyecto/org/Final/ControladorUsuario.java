a05cf784b49fc33a07b266ae2e9ced88863c0a41
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-04-22 16:20:18 -0500

    Domingo 22 de Abril 2018 Actualización 4:19pm

--- src/main/java/proyecto/org/Final/ControladorUsuario.java
+++ src/main/java/proyecto/org/Final/ControladorUsuario.java
@@ -1,15 +1,54 @@
 package proyecto.org.Final;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api")
+@RequestMapping("/api/usuario")
 public class ControladorUsuario {
 
     //Aqui van las 5 operaciones basicas
+    //Autowired 
+    @Autowired RepositorioUsuario repoUsu;
+
+    @CrossOrigin
+    @RequestMapping(value = {"/todos"}, method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public ArrayList<Usuario> todosUsuarios(){
+        return (ArrayList<Usuario>)repoUsu.findAll();
+    }
+
+    //primer metodo guardar
+    @RequestMapping(value = {"/guardar"},method = RequestMethod.POST, headers = {"Accept=application/json"})
+    public Estatus guardar(@RequestBody String json) throws Exception{
+
+       /*RequestBody envia un json*/
+
+       ObjectMapper maper= new ObjectMapper();
+       Usuario u=maper.readValue(json, Usuario.class);
+       Estatus e=new Estatus();
+       repoUsu.save(u);
+       e.setSuccess(true);
+       return e;
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = {"/actualizar"},method = RequestMethod.PUT, headers = {"Accept=application/json"})
+    public Estatus actualizar(@RequestBody String json) throws Exception{
+
+        /*RequestBody envia un json*/
+
+        ObjectMapper maper= new ObjectMapper();
+        Usuario u=maper.readValue(json, Usuario.class);
+        Estatus e=new Estatus();
+        repoUsu.findById(u.getIdUsuario());
+        repoUsu.save(u);
+        e.setSuccess(true);
+        return e;
+    }
 
 }
--- src/test/java/proyecto/org/Final/UsuariosTest.java
+++ src/test/java/proyecto/org/Final/UsuariosTest.java
@@ -1,11 +1,14 @@
 package proyecto.org.Final;
 
+import javafx.geometry.Pos;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,13 +31,51 @@ public class UsuariosTest {
 
         /*Generar un usuario con todos sus atributos incluyendo
         * un mensaje y una posicion*/
+        pos.setLatitud(19.90);
+        pos.setLongitud(100.0);
 
-        Assert.assertEquals(4,2+2);
+        mensa.setContenido("Holaaaa!!");
+
+        u.setEmail("luis.aquino.135769@gmail.com");
+		ArrayList<Mensaje> mensajes=new ArrayList<>();
+		mensajes.add(mensa);
+		u.setMensajes(mensajes);
+		u.setPassword("LaAc961115");
+		u.setNickName("LuisAbraham");
+		u.setPosicion(pos);
+
+		//Guardamos
+		Usuario guardado= repo.save(u);
+		Assert.assertEquals(guardado, u);
+
+        //Assert.assertEquals(4,2+2);
 
 	}
 
 	@Test
 	public void actualizar(){
+
+		Usuario u=new Usuario();
+		Posicion pos=new Posicion();
+		Mensaje mensa=new Mensaje();
+
+		pos.setLatitud(17.80);
+		pos.setLongitud(20.30);
+
+		mensa.setContenido("HOLAAAAA");
+
+		u.setIdUsuario("135769");
+		u.setEmail("luis.aquino.135769@gmail.com");
+		ArrayList<Mensaje> mensajes= new ArrayList<>();
+		mensajes.add(mensa);
+		u.setMensajes(mensajes);
+		u.setPassword("Androis852");
+		u.setNickName("LuisAbraham");
+		u.setPosicion(pos);
+
+		//Actualizamos
+		Usuario actualizar=repo.save(u);
+		Assert.assertEquals(actualizar, u);
 
         Assert.assertEquals(8,4+4);
 
@@ -50,12 +91,18 @@ public class UsuariosTest {
 	@Test
 	public void buscarTodo(){
 
+		int total= repo.findAll().size();
+		Assert.assertEquals(1,total);
+
 	    Assert.assertEquals(10, 5+5);
 
 	}
 
 	@Test
 	public void buscarPorId(){
+
+		Usuario usuarioBuscado=repo.findById("primero").get();
+		Assert.assertEquals("primero", usuarioBuscado.getIdUsuario());
 
 	    Assert.assertEquals(1,0+1);
 
